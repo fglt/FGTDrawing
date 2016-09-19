009665c6e63b542dec4593bedc7c29e0084bd9ff
Coding <coding@macbook-pro.local>
2016-09-19 22:06:47 +0800

    Initial Commit

---
+++ Drawing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drawing
+//
+//  Created by Coding on 9/19/16.
+//  Copyright © 2016 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drawing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drawing
+//
+//  Created by Coding on 9/19/16.
+//  Copyright © 2016 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
